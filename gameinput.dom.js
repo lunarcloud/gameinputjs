6ffbe40a401406c1f5bd3cca5b8e4c74257848aa
Sam Sarette <samuelsarette@linux.com>
2016-08-08 11:10:10 -0400

    Added License and check for main library in DOM addon

--- gameinput.dom.js
+++ gameinput.dom.js
@@ -1,4 +1,11 @@
+/**
+ * @preserve
+ * @source: https://raw.githubusercontent.com/lunarcloud/gameinputjs/master/gameinput.js
+ * @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT (Expat) License
+ */
 "use strict";
+
+if (typeof(GameInput) == "undefined") throw "GameInput JS must be included first!";
 
 var commonStyleElement = document.createElement('link');
 commonStyleElement.innerHTML = '<link rel="stylesheet" href="css/gameinput-common.css">';
