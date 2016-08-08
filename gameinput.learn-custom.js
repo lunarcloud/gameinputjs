1762dfd552fc6460fc0e50f2114e43cfcc87e1b0
Sam Sarette <samuelsarette@linux.com>
2016-08-08 11:10:29 -0400

    Created initial custom learned mapping js

---
+++ gameinput.learn-custom.js
@@ -0,0 +1,8 @@
+/**
+ * @preserve
+ * @source: https://raw.githubusercontent.com/lunarcloud/gameinputjs/master/gameinput.js
+ * @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT (Expat) License
+ */
+"use strict";
+
+if (typeof(GameInput) == "undefined") throw "GameInput JS must be included first!";
