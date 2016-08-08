b53e04bdee3b2b84dc6b3b808392d728d74698e9
Sam Sarette <samuelsarette@linux.com>
2016-08-08 11:43:11 -0400

    Fixed source header comments

--- gameinput.dom.js
+++ gameinput.dom.js
@@ -1,6 +1,6 @@
 /**
  * @preserve
- * @source: https://raw.githubusercontent.com/lunarcloud/gameinputjs/master/gameinput.js
+ * @source: https://raw.githubusercontent.com/lunarcloud/gameinputjs/master/gameinput.dom.js
  * @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT (Expat) License
  */
 "use strict";
