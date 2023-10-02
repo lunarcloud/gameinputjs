0bd67c5e40c6846b8fcf2db23d1fee737365b2b1
Samuel Sarette <samuelsarette@linux.com>
2023-10-01 22:53:27 -0400

    remove minifying

--- minify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -r dist
-mkdir dist
-npx uglifyjs --compress  --module --source-map --comments -o dist/gameinput.js  -- gameinput.js
-npx uglifyjs --compress  --module --source-map --comments -o dist/gameinput.models.js  -- gameinput.models.js
-npx uglifyjs --compress  --module --source-map --comments -o dist/gameinput.types.js  -- gameinput.types.js
-npx uglifyjs --compress  --module --source-map --comments -o dist/gameinput.dom.js  -- gameinput.dom.js
-
