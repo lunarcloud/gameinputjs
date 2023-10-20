35d7759cff550b65e380d4a145f020a025a808ca
Samuel Sarette <samuelsarette@linux.com>
2023-10-20 16:18:39 -0400

    make model definitions reference standard mapping where applicable

--- src/standard-gamepad-mapping.js
+++ src/standard-gamepad-mapping.js
@@ -6,19 +6,40 @@ import { AxisAsButton } from './axis-as-button.js'
  * @type {GamepadMapping}
  */
 const StardardGamepadMapping = new GamepadMapping(
-    13, 14, 15, 16,
-    10,
-    1, 2, 3, 4,
+    /* dPad */ 13, 14, 15, 16,
+    /* menu */ 10,
+    /* face */ 1, 2, 3, 4,
+    /* leftStick */
     new AxisAsButton(-2),
     new AxisAsButton(2),
     new AxisAsButton(-1),
     new AxisAsButton(1),
+    /* rightStick */
     new AxisAsButton(-4),
     new AxisAsButton(4),
     new AxisAsButton(-3),
     new AxisAsButton(3),
-    5, 6, 7, 8
+    /* shoulders */ 5, 6,
+    /* triggers */ 7, 8
+)
+
+const GravisGamepadProSchema = new GamepadMapping(
+    StardardGamepadMapping.leftStickUp,
+    StardardGamepadMapping.leftStickDown,
+    StardardGamepadMapping.leftStickLeft,
+    StardardGamepadMapping.leftStickRight,
+    StardardGamepadMapping.menu,
+    /* face */ 2, 3, 1, 4,
+    /* leftStick */ undefined, undefined, undefined, undefined,
+    StardardGamepadMapping.rightStickUp,
+    StardardGamepadMapping.rightStickDown,
+    StardardGamepadMapping.rightStickLeft,
+    StardardGamepadMapping.rightStickRight,
+    StardardGamepadMapping.leftShoulder,
+    StardardGamepadMapping.rightShoulder,
+    StardardGamepadMapping.leftTrigger,
+    StardardGamepadMapping.rightTrigger
 )
 
 export default StardardGamepadMapping
-export { StardardGamepadMapping }
+export { StardardGamepadMapping, GravisGamepadProSchema }
